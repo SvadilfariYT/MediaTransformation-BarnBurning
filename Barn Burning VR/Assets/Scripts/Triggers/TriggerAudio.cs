f05f60feac648f967d05748788a646693a579796
SvadilfariYT <dominik.sekular@gmail.com>
2019-12-11 17:57:24 +0100

    Audio via TriggerBoxes

---
+++ Barn Burning VR/Assets/Scripts/Triggers/TriggerAudio.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAudio : MonoBehaviour
+{
+    public string audioName;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        FindObjectOfType<AudioManager>().Play(audioName);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        FindObjectOfType<AudioManager>().Stop(audioName);
+    }
+}
